eae402d11e5527dd596132c17a3c1593ad4d226e
zarathezahid <zarazahid@uchicago.edu>
2022-03-31 15:50:40 -0500

    Create codeinclass2.py

---
+++ codeinclass2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print('Hello world')
+
+x = 10
+
+print(x*2)
+
+print('no more math')
+print('hello world class 2')
